b62cb6d282b21bf03cc96d7f8b4632de6d9733d7
Rasim Aghayev <rasimaqayev@gmail.com>
2025-03-21 14:43:43 +0400

    fic(frontEnd):Add missing: Implements frontend components and API integration.

---
+++ fe/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+
+    <Navbar />
+    <main class="container mx-auto p-4">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
